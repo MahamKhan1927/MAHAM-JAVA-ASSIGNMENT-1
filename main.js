7ebe7639522bd9d4052674aab78492631512f288
MahamKhan1927 <khanmaham2007192@gmail.com>
2024-12-29 22:19:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+alert("THANKYOU FOR VISITING");
+var a = prompt("Enter Your First Name");
+var b = prompt("Enter Your Second Name");
+var c = a + b;
+alert (c);
+alert("My Age Is 17");
+var a = 52;
+var b = 48;
+var c = a + b;
+alert (c);
+var a = prompt("Enter Your Email");
+var b = prompt("Enter Your Contact Number");
+var c = prompt("What Is Your Favourite Hobby?");
+var d = prompt("What Is Your Favourite Indoor Game?");
+var e = prompt("What Is Your Favorite Dish?");
+alert("THANK YOU")
